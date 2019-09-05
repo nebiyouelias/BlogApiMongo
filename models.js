5906d059353f38b029bbd60090f833303834ff32
Nebiyou Shiferaw <nebiyouelias04@gmail.com>
2019-09-04 21:57:40 -0400

    CRUD

--- models.js
+++ models.js
@@ -1,107 +1,33 @@
-"use strict";
+'use strict';
 
-const mongoose =require("mongoose");
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const blogPostsSchema = mongoose.Schema({
-  title: {type: String, required: true},
-  content:{type: String, required: true},
-  author:{
+const blogPostSchema = mongoose.Schema({
+  author: {
     firstName: String,
-    lastName: String,
+    lastName: String
   },
-  created:{type:Date, default: Date.now}
-}); 
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
 
 blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
- 
-blogPostsSchema.methods.serialize = function(){
+
+blogPostSchema.methods.serialize = function() {
   return {
     id: this._id,
-    title: this.title,
-    content: this.content,
     author: this.authorName,
-    created: this.date
+    content: this.content,
+    title: this.title,
+    created: this.created
   };
 };
 
-const BlogPost = mongoose.model("BlogPost", blogPostsSchema);
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
 module.exports = {BlogPost};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
