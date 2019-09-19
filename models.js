3e5aff60bd0a695366ccfb43736c6ed169eecefa
Nebiyou Shiferaw <nebiyouelias04@gmail.com>
2019-09-19 14:46:03 -0400

    authors and comments

--- models.js
+++ models.js
@@ -3,16 +3,35 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const blogPostSchema = mongoose.Schema({
-  author: {
-    firstName: String,
-    lastName: String
-  },
-  title: {type: String, required: true},
-  content: {type: String},
-  created: {type: Date, default: Date.now}
+
+var authorSchema = mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  userName: {
+    type: 'string',
+    unique: true
+  }
 });
 
+var commentSchema = mongoose.Schema({content: 'string'});
+
+const blogPostSchema = mongoose.Schema({
+  
+  title: 'string',
+  content: 'string',
+  author: {type: mongoose.Schema.Types.ObjectId, ref:'Author'},
+  comments: [commentSchema]
+});
+
+blogPostSchema.pre('find', function(next){
+  this.populate('author');
+  next();
+});
+
+blogPostSchema.pre('findOne', function(next){
+  this.populate('author');
+  next();
+});
 
 blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
@@ -28,6 +47,9 @@ blogPostSchema.methods.serialize = function() {
   };
 };
 
-const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+var Author = mongoose.model('Author', authorSchema);
+const BlogPost =mongoose.model('BlogPost',blogPostSchema);
 
-module.exports = {BlogPost};
+
+
+module.exports = {Author,BlogPost};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 const { DATABASE_URL, PORT } = require('./config');
@@ -13,59 +14,114 @@ const app = express();
 app.use(morgan('common'));
 app.use(express.json());
 
-app.get('/posts', (req, res) => {
-  BlogPost
+
+app.get('/authors',(req,res)=>{
+  Author 
     .find()
-    .then(posts => {
-      res.json(posts.map(post => post.serialize()));
+    .then(authors => {
+      res.json(authors.map(author =>{
+        return {
+          id: author._id,
+          name: `${author.firstName} ${author.lastName}`,
+          userName: author.userName
+        };
+      }));
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
+      res.status(500).json({error:'Something went terrably wrong'});
     });
-});
+}); 
 
-app.get('/posts/:id', (req, res) => {
-  BlogPost
-    .findById(req.params.id)
-    .then(post => res.json(post.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'something went horribly awry' });
-    });
-});
+app.post('/authors',(req,res) => {
+  const requiredFields = ['firstName','lastName','userName'];
+  requiredFields.firEach(field => {
+    if(!(field in req.body)){
+      const message = `Missing \`${field}\`in request body`;
+      console.error(message);
+      return res.status(400).send(message)
+    }
+  });
 
-app.post('/posts', (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i = 0; i < requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
+  Author
+  .findOne({userName: req.body.userName})
+  .then(author => {
+    if (author){
+      const message = `Username already taken`;
       console.error(message);
       return res.status(400).send(message);
     }
-  }
+    else {
+      Author 
+        .create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          userName: req.body.userName
+        })
+        .then(author => res.status(201).json({
+          _id: author.id,
+          name: `${author.firstName} ${author.Lastname}`,
+          userName: author.userName
+        }))
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({error: `Something went wrong terrably wrong`});
+        });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error:'Something went horribly wrong'});
+  });
+}); 
 
-  BlogPost
-    .create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author
-    })
-    .then(blogPost => res.status(201).json(blogPost.serialize()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: 'Something went wrong' });
+app.put('/authors/:id',(req,res) =>{
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)){
+    res.status(400).json({
+      error: 'Request Path id and request body id valus must match'
     });
+  }
+  const updated = {};
+  const updateableFields = ['firstName', 'lastName','userName'];
+  updateableFields.forEach(field => {
+    if(field in req.body){
+      updated[field] = req.body[field];
+    }
+  });
 
+  Author 
+    .findOne({userName: updated.userName || '', _id: {$ne: req.params.id} })
+    .then(author => {
+      if(author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else{
+        Author 
+        .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+        .then(updatedAuthor => {
+          res.status(200).json({
+            id: updatedAuthor.id,
+            name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+            userName: updatedAuthor.userName
+          });
+        })
+        .catch(err => res.status(500).json({message: err}));
+      }
+    });
 });
 
-
-app.delete('/posts/:id', (req, res) => {
+app.delete('/authors/:id', (req, res) => {
   BlogPost
-    .findByIdAndRemove(req.params.id)
+    .remove({ author: req.params.id })
     .then(() => {
-      res.status(204).json({ message: 'success' });
+      Author
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+          console.log(`Deleted blog posts owned by and author with id \`${req.params.id}\``);
+          res.status(204).json({ message: 'success' });
+        });
     })
     .catch(err => {
       console.error(err);
@@ -73,42 +129,123 @@ app.delete('/posts/:id', (req, res) => {
     });
 });
 
+app.get('/blogposts', (req, res) => {
+  BlogPost
+    .find()
+    .then(posts => {
+      res.json(posts.map(post => {
+        return {
+          id: post._id,
+          author: post.authorName,
+          content: post.content,
+          title: post.title
+        };
+      }));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Something went terribly wrong'});
+    });
+  });
+    
+app.get('/posts/:id', (req, res) => {
+  BlogPost
+  .findById(req.params.id)
+  .then(post => {
+    res.json({
+      id: post._id,
+      author: post.authorName,
+      content: post.content,
+      title: post.title,
+      comments: post.comments
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'Something went horrobly wrong'});
+  });
+});
 
-app.put('/posts/:id', (req, res) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+app.post('/posts/', (req,res) =>{
+  const requiredFields =['title','content','author_id'];
+  requiredFields.forEach(field => {
+    if(!(field in req.body)){
+      const message= `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  });
+  Author
+  .findById(req.body.author_id)
+  .then(author => {
+    if(author) {
+      BlogPost
+        .create({
+          title: req.body.title,
+          content: req.body.content,
+          author: req.body.id
+        })
+        .then(blogPost => res.status(201).json({
+          id: blogPost.id,
+          author: `${author.firstName} ${author.lastName}`,
+          content: blogPost.content,
+          title: blogPost.title,
+          comments: blogPost.comments
+        }))
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({errpr: 'Something went terrably wrong'});        
+        });
+    }
+    else {
+      const message = `Author not found`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'Something went terrably wrong'});
+  });
+});
+
+app.put('/posts/:id',(req,res) => {
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)){
     res.status(400).json({
-      error: 'Request path id and request body id values must match'
+      error: 'Request Path id and request body id valus must match'
     });
   }
-
-  const updated = {};
-  const updateableFields = ['title', 'content', 'author'];
+  const updated ={};
+  const updateableFields =['title', 'content'];
   updateableFields.forEach(field => {
     if (field in req.body) {
       updated[field] = req.body[field];
     }
   });
 
+  BlogPost 
+  .findByIdAndUpdate(req.params.id, {$set: updated},{new:true})
+  .then(updatedPost => res.status(200).json({
+    id: updatedPost.id,
+    title: updatedPost.title,
+    content: updatedPost.content
+  }))
+  .catch(err => res.status(500).json({message:err}));
+}); 
+
+app.delete('/posts/:id', (req, res) => {
   BlogPost
-    .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-    .then(updatedPost => res.status(204).end())
-    .catch(err => res.status(500).json({ message: 'Something went wrong' }));
+  .findByIdAndRemove(req.params.id)
+  .then(() => {
+    console.log(`Deleted blog post with id \`${req.params.id}\``);
+    res.status(204).end();
+  });
 });
 
-
-app.delete('/:id', (req, res) => {
-  BlogPost
-    .findByIdAndRemove(req.params.id)
-    .then(() => {
-      console.log(`Deleted blog post with id \`${req.params.id}\``);
-      res.status(204).end();
-    });
+app.use('*',function(req, res){
+  res.status(404).json({message:'Not found'});
 });
 
-
-app.use('*', function (req, res) {
-  res.status(404).json({ message: 'Not Found' });
-});
 
 // closeServer needs access to a server object, but that only
 // gets created when `runServer` runs, so we declare `server` here
